e4dd7f386eb883760658dcaafb7b0bb47c535a73
irfan farid <irfarid3@gmail.com>
2020-02-15 15:09:33 +0700

    penambahan command

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const Telegraf = require('telegraf')
 
+const bot = new Telegraf('981900501:AAEEm2aDvZGM0wWVyKV4ACUUuB_fYC95v0Y')
+bot.start((ctx) => ctx.reply('Selamat Datang!'))
+bot.help((ctx) => ctx.reply('minta tolong apa ?'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hai', (ctx) => ctx.reply('Haiiiiii <3'))
+bot.launch()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+ });
