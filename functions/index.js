4d9857fd0b6a0b9b465ffde2597abb8eb079c750
irfan farid <irfarid3@gmail.com>
2020-02-16 20:16:57 +0700

    Tambah Command

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,10 @@ bot.start((ctx) => ctx.reply('Selamat Datang!'))
 bot.help((ctx) => ctx.reply('minta tolong apa ?'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hai', (ctx) => ctx.reply('Haiiiiii <3'))
+bot.hears('/about', (ctx) => ctx.reply('Ini adalah Simple Chat Bot yang dibuat untuk sarana belajar mengenai cara pembuatan Bot menggunakan NodeJs dan Firebase Cloud Functions'))
+bot.hears('/creator', (ctx) => ctx.reply('Bot ini diciptkan oleh Irfan Farid'))
+bot.hears('/how', (ctx) => ctx.reply('Bot ini dibuat menggunakan NodeJs dan Firebase Cloud Functions'))
+bot.hears('/link', (ctx) => ctx.reply('https://gitlab.com/irfarid3/telegram-bot-dengan-nodejs-dan-firebase-cloud'))
 bot.launch()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
